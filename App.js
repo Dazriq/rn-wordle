592169af9b9f666c2c86079c85f6757f68e34099
Dazriq <dazriqiqramie@gmail.com>
2022-02-22 20:08:36 +0800

    Added win prompt and animations

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { useState} from 'react';
+import { useState, useRef} from 'react';
 import { 
   StyleSheet, 
   Text, 
   View, 
   SafeAreaView, 
+  TouchableWithoutFeedback, 
   TouchableHighlight, 
   TextInput, 
   Dimensions, 
   TouchableOpacity, 
+  Animated, 
+  Vibration, 
+  Button, 
 } from 'react-native';
 import {keyboardData} from './keyboard.js';
+import { scanDictionary } from './dictionary-eng.js';
 
 export default function App() {
   let textArray = []
@@ -24,13 +29,78 @@ export default function App() {
   }
   const [letters, setLetters] = useState(textArray);
   const [line, setLine] = useState(0);
-  const [input, setInput] = useState('');
-  const [wordRight, setWordRight] = useState('LUKIS');
+  const [wordRight, setWordRight] = useState('WORDS');
   const [keyPress, setKeypress] = useState('');
   const [keyboard, setKeyboard] = useState([...keyboardData]); 
+  const [dictionary, setDictionary] = useState(dictionary);
+  const [blurScreen, setBlurScreen] = useState(false); 
+  const [keyboardVisibility, setKeyboardVisibility] = useState(true); 
+  const [winTextVisibility, setwinTextVisibility] = useState(false); 
+
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const winTextAnimPosition = useRef(new Animated.Value(0)).current;
+  const winTextAnimOpacity = useRef(new Animated.Value(0)).current;
+  
+  const fadeInOut = () => {
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 10, 
+      useNativeDriver: false, 
+    }).start();
+    setTimeout(() => {
+      Animated.timing(fadeAnim, {
+        toValue: 0,
+        duration: 2000, 
+        useNativeDriver: false, 
+      }).start();
+    }, 10);
+  };
+
+  const winTextPrompt = () => {
+    //Vibration.vibrate(500); 
+    setwinTextVisibility(true);   
+    Vibration.vibrate([15,500], false);
+    //blur the background
+    setBlurScreen(true); 
+    setKeyboardVisibility(false);
+    // Will change fadeAnim value to 1 in 5 seconds
+    Animated.timing(winTextAnimOpacity, {
+      toValue: 1, 
+      duration: 500, 
+      useNativeDriver: false, 
+    }).start();
+    Animated.spring(winTextAnimPosition, {
+      toValue: (windowHeight/2 - windowHeight * 0.3/2),
+      useNativeDriver: false, 
+      duration: 1000,  
+    }).start();
+  };
+
+  const winTextretrieve = () => {
+    //TODO:retrieve wintext when blurred background is pressed
+    //unblur the background
+    if (winTextVisibility) {
+      setwinTextVisibility(false); 
+      Animated.timing(winTextAnimOpacity, {
+        toValue: 0, 
+        duration: 2, 
+        useNativeDriver: false, 
+      }).start();
+    }
+    else {
+      setwinTextVisibility(true);
+      Animated.timing(winTextAnimOpacity, {
+        toValue: 1, 
+        duration: 2, 
+        useNativeDriver: false, 
+      }).start();
+    }
+
+  };
+
 
   const keyHandler = (newText) => {
-    setInput(newText);
     var j = 0;
     var textArrayCopy = letters;
     for (let i = line * 5; i < (line * 5) + 5; i++) {
@@ -53,72 +123,118 @@ export default function App() {
     if (keyPress.length < 5) {
       return; 
     }
-
-    var text = keyPress.substring(0, 5);
-    setKeypress('');
-
-    if (text.localeCompare(wordRight)) {
-      //if won
-    }
-
-    //get the result of each input placed by the user
-    var startingIndex = line * 5;
-    var textArrayCopy = letters;
     
-    var j = 0;
-    var indexKeyboard;
-    var keyboardCopy = [...keyboard]; 
-    for (var i = startingIndex; i < startingIndex + 5; i++) {
-      var isRightNew = false; 
-      var ispositionRightNew = false;
-      var char = letters[i].value;
+    var text = keyPress.substring(0, 5);
+    
+    //check whether inside dictionary or not
+    var isWord = scanDictionary(text); 
+    if (!isWord) {
+      fadeInOut(); 
+      return; 
+    }
+    //if not inside dictionary
+    else {
+      setKeypress('');
 
-      for (let k = 0; k < keyboard.length; k++) {
-        if (keyboard[k].value == char) {
-          indexKeyboard = k; 
-          break;
+      if (text === wordRight) {
+        winTextPrompt();
+      }
+
+      //get the result of each input placed by the user
+      var startingIndex = line * 5;
+      var textArrayCopy = letters;
+      
+      var j = 0;
+      var indexKeyboard;
+      var keyboardCopy = [...keyboard]; 
+      for (var i = startingIndex; i < startingIndex + 5; i++) {
+        var isRightNew = false; 
+        var ispositionRightNew = false;
+        var char = letters[i].value;
+
+        for (let k = 0; k < keyboard.length; k++) {
+          if (keyboard[k].value == char) {
+            indexKeyboard = k; 
+            break;
+          }
         }
-      }
-      console.log(indexKeyboard);
-      var color; 
-      //if correct 
-      if (char == wordRight.charAt(j)) {
-        textArrayCopy[i].isRight = true; 
-        color = '#538D4E'; 
-      }
-      //if position wrong
-      else if (wordRight.indexOf(char) > -1) {
-        textArrayCopy[i].isPositionRight = true; 
-        color = '#B59F3B'; 
-      }
-      //if both wrong
-      else {
-        textArrayCopy[i].isRight = false; 
-        textArrayCopy[i].isPositionRight = false; 
-        color = '#3A3A3C'; 
-      }
-      textArrayCopy[i].color = color; 
-      keyboardCopy[indexKeyboard].color = color;
-      j++;
-    };
+        var color; 
+        //if correct 
+        if (char == wordRight.charAt(j)) {
+          textArrayCopy[i].isRight = true; 
+          color = '#538D4E'; 
+        }
+        //if position wrong
+        else if (wordRight.indexOf(char) > -1) {
+          textArrayCopy[i].isPositionRight = true; 
+          color = '#B59F3B'; 
+        }
+        //if both wrong
+        else {
+          textArrayCopy[i].isRight = false; 
+          textArrayCopy[i].isPositionRight = false; 
+          color = '#3A3A3C'; 
+        }
+        textArrayCopy[i].color = color; 
+        keyboardCopy[indexKeyboard].color = color;
+        j++;
+      };
 
-    setLetters([...textArrayCopy]);
-    setLine(line + 1);
-    setKeyboard([...keyboardCopy]);
-    setKeypress(''); 
+      setLetters([...textArrayCopy]);
+      setLine(line + 1);
+      setKeyboard([...keyboardCopy]);
+      setKeypress(''); 
+    }
   }
 
   const keyPressHandler = (key) => {
-    if (key == '✓') {
-      submitHandler(); 
+    //if backspace is pressed
+    if (key == '⌫') {  
+      console.log(letters);
+      var letterIndex; 
+      if (letters[0].value === '') {
+        return; 
+      }
+      for (let i = letters.length - 1; i >= 0; i--) {
+        if (letters[i].value != '') {
+          letterIndex = i;
+          break; 
+        }
+      }
+      console.log(letterIndex); 
+      console.log(line * 5 - 1); 
+      if (letterIndex == (line * 5 - 1)) {
+        return; 
+      }
+      
+      var keyPressCopy = keyPress; 
+      keyPressCopy = keyPressCopy.substring(0, keyPressCopy.length - 1); 
+      setKeypress(keyPressCopy); 
+
+      var lettersCopy = letters; 
+      lettersCopy[letterIndex].value = ''; 
+      
+      setLetters([...lettersCopy]); 
       return; 
     }
-    var keyPressCopy = keyPress + key; 
-    setKeypress(keyPressCopy);
-    keyHandler(keyPressCopy);
+    //if enter is pressed
+    else if (key == '✓') {
+      submitHandler(); 
+      return; 
+    } 
+    else {
+      var keyPressCopy = keyPress;
+      keyPressCopy = keyPressCopy + key;  
+      setKeypress(keyPressCopy);
+      keyHandler(keyPressCopy);
+    }
   } 
   return (
-    <SafeAreaView style={styles.container}>                         
+    <SafeAreaView style={styles.container}>   
+      { blurScreen && <TouchableHighlight onPress={() => winTextretrieve} style={styles.blurContainer}>
+        <Text style={{height: '100%', width: '100%'}}>Hello world</Text>
+      </TouchableHighlight>
+      }
       <Text style={styles.title}>Infinite Wordle</Text>
       <View style={styles.wordsContainer}>
         {letters.map((letter, index) => {
@@ -149,7 +265,8 @@ export default function App() {
         }     
         )}
       </View>  
-      <View style={styles.keyboard}>
+      {keyboardVisibility && 
+        <View style={styles.keyboard}>
         {keyboard.map((key, index) => {
           return (
             <View style={{
@@ -176,7 +293,33 @@ export default function App() {
             </View>
           );
         })}
-      </View>
+        </View>
+      }
+
+      <Animated.View
+        style={[
+          styles.fadingContainer,
+          {
+            // Bind opacity to animated value
+            opacity: fadeAnim
+          }
+        ]}
+      >
+        <Text style={styles.fadingText}>Not in word list!</Text>
+      </Animated.View>
+
+      <Animated.View
+        style={[
+          styles.winTextContainer, {
+            // Bind opacity to animated value
+            top: winTextAnimPosition, 
+            opacity: winTextAnimOpacity, 
+          }
+        ]}
+      >
+        <Text style={styles.winText}>🆆🅾🅽</Text>
+        <Text style={styles.reloadIcon}>↻</Text>
+      </Animated.View>
 
     </SafeAreaView>
   );
@@ -192,19 +335,21 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
     paddingTop: 40, 
   },
-  promptTextWin: {
+
+  fadingContainer: {
+    backgroundColor: '#ffffff', 
+    position: 'absolute', 
+    borderRadius: 5, 
+    top: 0.3 * windowHeight, 
+    elevation: 3, 
+  },
+  fadingText: {
+    fontSize: 20, 
+    fontFamily: 'sans-serif', 
     padding: 10, 
-    fontSize: 70, 
-    fontFamily: 'Roboto',
-    fontWeight: 'bold',  
-    color: '#80ed99', 
-  }, 
-  promptTextNext: {
-    color: '#7B7B7B', 
-    padding: 10, 
-    fontSize: 25, 
-    fontFamily: 'Roboto', 
-  }, 
+    color: '#121213'
+  },
+
   title: {
     fontSize: 50, 
     paddingBottom: 5, 
@@ -212,15 +357,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold', 
     color: '#ffffff', 
     
-  }, 
-  invisibleInput: {
-    height: '50%', 
-    width: '90%',
-    backgroundColor: 'transparent', 
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    position: 'absolute',
-    color: 'transparent', 
   }, 
   wordsContainer: {
     height: windowHeight * 0.5, 
@@ -261,6 +397,43 @@ const styles = StyleSheet.create({
     bottom: 0, 
     flexDirection: 'row', 
     flexWrap: 'wrap'
+  }, 
+  
+  blurContainer: {
+    top: '5%', 
+    height: '100%',
+    width: '100%',  
+    backgroundColor: '#121213', 
+    position: 'absolute', 
+    elevation: 4, 
+    opacity: 0.8, 
+  },
+
+  winTextContainer: {
+    backgroundColor: '#501537', 
+    height: windowHeight * 0.3, 
+    width: windowWidth * 0.8, 
+    position: 'absolute', 
+    elevation: 5, 
+    borderRadius: windowWidth * 0.8 / 5, 
+    borderWidth: 5, 
+    borderColor: 'rgba(0, 0, 0, 0.5)', 
+  }, 
+
+  winText: {  
+    color: '#ebb9df', 
+    fontSize: 70, 
+    fontFamily: 'sans-serif-medium', 
+    textAlign: 'center', 
+    top: '10%', 
+    letterSpacing: 3, 
+  }, 
+
+  reloadIcon: {
+    color: '#ebb9df', 
+    fontSize: 100, 
+    fontFamily: 'sans-serif-medium', 
+    textAlign: 'center', 
   }
   //green = #538D4E
   //yellow = #B59F3B
